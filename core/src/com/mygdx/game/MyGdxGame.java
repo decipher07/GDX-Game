f73a5e5e9a3615a172e26b9d8e0648b6da16f656
decipher07 <social.deej@gmail.com>
2020-05-02 04:25:54 +0530

    Able to Setup Coins on The Screen

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background ;
@@ -13,28 +16,60 @@ public class MyGdxGame extends ApplicationAdapter {
 	int manState = 0 ;
 	int pause = 0 ;
 	int manY = 0;
-	float gravity = 0.2f ;
+	float gravity = 0.5f ;
 	float velocity = 0 ;
+
+	ArrayList<Integer> coinXs = new ArrayList<Integer>();
+	ArrayList<Integer> coinYs = new ArrayList<Integer>();
+    Texture coin ;
+	int coinCount ;
+	Random random ;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+
 		background = new Texture("bg.png");
+
 		man = new Texture[4];
 		man[0] = new Texture("frame-1.png");
 		man[1] = new Texture("frame-2.png");
 		man[2] = new Texture("frame-3.png");
 		man[3] = new Texture("frame-4.png");
+
 		manY = Gdx.graphics.getHeight()/2 ;
+
+		coin = new Texture("coin.png");
+	    random = new Random();
 	}
+
+	public void makeCoin(){
+	    float height = random.nextFloat() * Gdx.graphics.getHeight();
+
+	    coinYs.add((int) height);
+
+	    coinXs.add(Gdx.graphics.getWidth());
+    }
 
 	@Override
 	public void render () {
 		batch.begin();
 		batch.draw(background, 0, 0 , Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
+		if (coinCount < 100){
+		    coinCount++ ;
+        } else {
+		    coinCount = 0 ;
+		    makeCoin();
+        }
+
+		for (int i = 0 ; i < coinXs.size() ; i++ ){
+		    batch.draw(coin, coinXs.get(i) , coinYs.get(i));
+		    coinXs.set(i , coinXs.get(i) - 4 );
+        }
+
 		if (Gdx.input.justTouched()){
-			velocity -= 10 ; 
+			velocity -= 10 ;
 		}
 
 		if (pause < 20){
